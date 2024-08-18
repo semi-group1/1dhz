4af824b8d789fecbe13ab4f29e8ba8b42a38896b
junmipark <junmipark1@gmail.com>
2024-08-19 00:48:28 +0900

    기능구현: 관리자 페이지 SO 모델 작성

---
+++ semiProject/src/main/java/semi/model/AdminManagementService.java
@@ -0,0 +1,105 @@
+package semi.model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import semi.model.dao.*;
+
+public class AdminManagementService {
+	@Autowired
+	AdminUserDAO adminUserDAO;
+	@Autowired
+	AdminTopicDAO adminTopicDAO;
+	@Autowired
+	AdminReportDAO adminReportDAO;
+
+	public List<AdminUser> selectAllUserInfos() {
+		return adminUserDAO.selectAllUserInfos();
+	}
+
+	public AdminUser selectUserInfo(int userId) {
+		return adminUserDAO.selectUserInfo(userId);
+	}
+
+	@Transactional
+	public boolean setUserInactive(int userId, String desc, int length) {
+		boolean result = false;
+
+		if (adminUserDAO.isUserInactive(userId) == 1) {
+			return result;
+		}
+
+		if (adminUserDAO.insertUserInactive(userId, desc, length) == 1
+				&& adminUserDAO.updateUserStatus(userId, AdminUserStatus.inactive) == 1) {
+			result = true;
+		}
+
+		return result;
+	}
+
+	public List<AdminTopic> selectAllTopics() {
+		return adminTopicDAO.selectAllTopics();
+	}
+
+	@Transactional
+	public boolean setPostInactive(int postId, String desc) {
+		boolean result = false;
+
+		if (adminTopicDAO.isPostInactive(postId) == 1) {
+			return false;
+		}
+
+		if (adminTopicDAO.insertPostInactive(postId, desc) == 1
+				&& adminTopicDAO.updatePostStatus(postId, AdminPostStatus.inactive) == 1) {
+			result = true;
+		}
+
+		return result;
+	}
+
+	public List<AdminReport> selectReportsByType(String type) {
+		List<AdminReport> adminReports = null;
+		AdminReportType statusType = null;
+
+		switch (type) {
+		case "post":
+			statusType = AdminReportType.post;
+			break;
+		case "user":
+			statusType = AdminReportType.user;
+			break;
+		case "comment":
+			statusType = AdminReportType.comment;
+			break;
+		}
+
+		adminReports = adminReportDAO.selectReportsByType(statusType);
+
+		return adminReports;
+	}
+
+	public boolean updateReportStatus(int reportId, String status) {
+		boolean result = false;
+		AdminReportStatus statusType = null;
+
+		switch (status) {
+		case "inProgress":
+			statusType = AdminReportStatus.inProgress;
+			break;
+		case "completed":
+			statusType = AdminReportStatus.completed;
+			break;
+		case "denied":
+			statusType = AdminReportStatus.denied;
+			break;
+		}
+
+		if (adminReportDAO.updateReportStatus(reportId, statusType) == 1) {
+			result = true;
+		}
+
+		return result;
+	}
+}
