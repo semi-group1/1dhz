c67dc96281e4edcfb06c48a331a5658d62581bb9
Kwon SunSuk <yangho9qt@gmail.com>
2024-08-20 16:17:08 +0900

    820/16:00

---
+++ semiProject/src/main/java/semi/model/dao/BoardDao.java
@@ -0,0 +1,57 @@
+package semi.model.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import semi.model.BoardDto;
+import semi.Form.BoardForm;
+import semi.Form.CommonForm;
+
+@Repository
+public class BoardDao {
+
+	@Resource(name = "sqlSession")
+	private SqlSession sqlSession;
+
+	private static final String NAMESPACE = "com.spring.board.boardMapper";
+
+	/** 게시판 - 목록 수 */
+	public int getBoardCnt(BoardForm boardForm) throws Exception {
+		return sqlSession.selectOne(NAMESPACE + ".getBoardCnt", boardForm);
+	}
+
+	/** 게시판 - 목록 조회 */
+	public List<BoardDto> getBoardList(BoardForm boardForm) throws Exception {		
+		return sqlSession.selectList(NAMESPACE + ".getBoardList", boardForm);
+	}
+
+	/** 게시판 - 그룹 번호 조회 */
+	public int getBoardReRef(BoardForm boardForm) throws Exception {
+		return sqlSession.selectOne(NAMESPACE + ".getBoardReRef", boardForm);
+	}
+	
+	/** 게시판 - 등록 */
+	public int insertBoard(BoardForm boardForm) throws Exception {
+		return sqlSession.insert(NAMESPACE + ".insertBoard", boardForm);
+	}
+
+	/** 게시판 - 삭제 */
+	public int deleteBoard(BoardForm boardForm) throws Exception {
+		return sqlSession.delete(NAMESPACE + ".deleteBoard", boardForm);
+	}
+
+	/** 게시판 - 수정 */
+	public int updateBoard(BoardForm boardForm) throws Exception {
+		return sqlSession.update(NAMESPACE + ".updateBoard", boardForm);
+	}
+	
+	/** 게시판 - 답글 등록 */
+	public int insertBoardReply(BoardForm boardForm) throws Exception {
+		return sqlSession.insert(NAMESPACE + ".insertBoardReply", boardForm);
+	}
+	
+}
