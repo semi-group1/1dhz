6f88874fbddb6f66651996d9ff83d54365237474
junmipark <junmipark1@gmail.com>
2024-08-21 14:53:03 +0900

    활성화된 회원이 아닐 시 메인 페이지로 redirect하게 수정

--- semiProject/src/main/java/semi/controller/UserController.java
+++ semiProject/src/main/java/semi/controller/UserController.java
@@ -12,65 +12,62 @@ import semi.model.so.UserService;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	HelloDao dao;
-	
+
 	@Autowired
 	UserService us;
-	
+
 	@RequestMapping("/myPage/{userId}")
 	public String myPage(@PathVariable("userId") int userId, Model model) {
 		User user = us.selectOneUser(userId);
-		if(!user.getUser_status().equals("active")) {
-			model.addAttribute("msg", "테스트 입니다.");
-			model.addAttribute("emp", dao.selectAll());
-			return "hello";
-		}else {
+		if (!user.getUser_status().equals("active")) {
+			return "redirect: /main";
+		} else {
 			model.addAttribute("user", user);
 			model.addAttribute("user_post", us.selectAllUserPost(userId));
-			
+
 			return "myPage";
 		}
 	}
-	
+
 	@RequestMapping("/editInfo/{userId}")
 	public String editInfo(@PathVariable("userId") int userId, Model model) {
 		model.addAttribute("user", us.selectOneUser(userId));
-		
+
 		return "editInfo";
 	}
-	
+
 	@RequestMapping("/userDelete/{userId}")
 	public String deleteUser(@PathVariable("userId") int userId, Model model) {
-		if(us.isDeleteSuccess(userId)) {
+		if (us.isDeleteSuccess(userId)) {
 			return "toMain";
-		}else {
+		} else {
 			model.addAttribute("user", us.selectOneUser(userId));
 			model.addAttribute("user_post", us.selectAllUserPost(userId));
 			return "myPage";
 		}
-		
-		
+
 	}
-	
-	@RequestMapping(value="/editInfo/editInfoProcess", method=RequestMethod.POST)
-	public String editInfoValidation(Model model, UserInfoDto dto) {		
-		if(us.isEditValidate(dto)) {			
-			if(us.isEditSuccess(dto)) {
+
+	@RequestMapping(value = "/editInfo/editInfoProcess", method = RequestMethod.POST)
+	public String editInfoValidation(Model model, UserInfoDto dto) {
+		if (us.isEditValidate(dto)) {
+			if (us.isEditSuccess(dto)) {
 				model.addAttribute("user", us.selectOneUser(dto.getId()));
 				model.addAttribute("user_post", us.selectAllUserPost(dto.getId()));
 				return "myPage";
-			}else {
+			} else {
 				model.addAttribute("user", us.selectOneUser(dto.getId()));
 				model.addAttribute("msg", "데이터 수정 오류");
 				return "editInfo";
 			}
-		}else {
+		} else {
 			model.addAttribute("user", us.selectOneUser(dto.getId()));
 			model.addAttribute("msg", "비밀번호가 일치하지 않습니다.");
 			return "editInfo";
 		}
 	}
-	
+
 }
