de394f801061267d5d0e90cd349f8ad792675c75
junmipark <junmipark1@gmail.com>
2024-08-21 14:41:37 +0900

    오류 수정

--- semiProject/src/main/java/semi/model/dao/UserDao.java
+++ semiProject/src/main/java/semi/model/dao/UserDao.java
@@ -23,9 +23,10 @@ public class UserDao {
 	public UserDao(DataSource ds) {
 		this.ds = ds;
 	}
-	
+
 	public User selectOneUser(int id) {
-		this.sql = "select user_id, user_name, user_email, job_category_super, job_category_sub, user_comment, user_profile_image, user_status from semi_user join semi_job_category on semi_user.user_job = semi_job_category.job_category_id where user_id=" + id;
+		this.sql = "select user_id, user_name, user_email, job_category_super, job_category_sub, user_comment, user_profile_image, user_status from semi_user join semi_job_category on semi_user.user_job = semi_job_category.job_category_id where user_id="
+				+ id;
 		User user = new User();
 		try {
 			conn = ds.getConnection();
@@ -52,8 +53,7 @@ public class UserDao {
 				e2.printStackTrace();
 			}
 		}
-		
-		
+
 		return user;
 	}
 
@@ -89,11 +89,12 @@ public class UserDao {
 
 		return userList;
 	}
-	
-	public List<User_Post> selectAllUserPost(int id){
+
+	public List<User_Post> selectAllUserPost(int id) {
 		List<User_Post> upList = new ArrayList<User_Post>();
-		this.sql = "select post_id, job_category_super, post_title, post_date from semi_user su join semi_topic st on su.user_id = st.user_id join semi_job_category sjc on su.user_job = sjc.job_category_id where su.user_id = " + id;
-		
+		this.sql = "select post_id, job_category_super, post_title, post_date from semi_user su join semi_topic st on su.user_id = st.user_id join semi_job_category sjc on su.user_job = sjc.job_category_id where su.user_id = "
+				+ id;
+
 		try {
 			conn = ds.getConnection();
 			stmt = conn.createStatement();
@@ -103,7 +104,8 @@ public class UserDao {
 				User_Post up = new User_Post();
 
 				up.setPost_id(rs.getInt("post_id"));
-				up.setJob_category_super(rs.getString("job_category_super"));;
+				up.setJob_category_super(rs.getString("job_category_super"));
+				;
 				up.setPost_title(rs.getString("post_title"));
 				up.setPost_date(rs.getDate("post_date"));
 
@@ -120,15 +122,15 @@ public class UserDao {
 				e2.printStackTrace();
 			}
 		}
-		
+
 		return upList;
 	}
-	
+
 	public boolean updateUserInfo(UserInfoDto dto) {
-		boolean result = true;
-		
+		int rowNum = 0;
+
 		this.sql = "update semi_user set user_email=?, user_pw=?, user_name=?, user_comment=? where user_id=?";
-		
+
 		try {
 			conn = ds.getConnection();
 			pstmt = conn.prepareStatement(sql);
@@ -137,41 +139,44 @@ public class UserDao {
 			pstmt.setString(3, dto.getName());
 			pstmt.setString(4, dto.getComment());
 			pstmt.setInt(5, dto.getId());
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				
-			}
+
+			rowNum = pstmt.executeUpdate();
 
 		} catch (Exception e) {
 			e.printStackTrace();
-			result = false;
 		} finally {
 			try {
+				if (stmt != null && !stmt.isClosed()) {
+					stmt.close();
+				}
+				if (pstmt != null && !pstmt.isClosed()) {
+					pstmt.close();
+				}
 				if (!conn.isClosed()) {
 					conn.close();
 				}
 			} catch (Exception e2) {
 				e2.printStackTrace();
-				result = false;
+				return false;
 			}
 		}
 
-		return true;
+		return rowNum > 0;
 	}
-	
+
 	public boolean deleteUser(int userId) {
 		boolean result = true;
-		
+
 		this.sql = "update semi_user set user_status=? where user_id=?";
-		
+
 		try {
 			conn = ds.getConnection();
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, "deleted");
 			pstmt.setInt(2, userId);
 			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				
+			while (rs.next()) {
+
 			}
 
 		} catch (Exception e) {
