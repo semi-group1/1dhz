4279abb1a63255cb2e0425dc4dcf6ee7b9c27356
Kwon SunSuk <yangho9qt@gmail.com>
2024-08-21 13:53:31 +0900

    08211353

--- semiProject/src/main/java/semi/model/dao/BoardDao.java
+++ semiProject/src/main/java/semi/model/dao/BoardDao.java
@@ -10,37 +10,4 @@ import semi.model.BoardDto;
 @Repository
 public class BoardDao {
 
-	@Resource(name = "DataSource ")
-	private DataSource DataSource;
-
-	private static final String NAMESPACE = "com.spring.board.boardMapper";
-
-	public int getBoardCnt(BoardForm boardForm) throws Exception {
-		return sqlSession.selectOne(NAMESPACE + ".getBoardCnt", boardForm);
-	}
-
-	public List<BoardDto> getBoardList(BoardForm boardForm) throws Exception {		
-		return sqlSession.selectList(NAMESPACE + ".getBoardList", boardForm);
-	}
-
-	public int getBoardReRef(BoardForm boardForm) throws Exception {
-		return sqlSession.selectOne(NAMESPACE + ".getBoardReRef", boardForm);
-	}
-	
-	public int insertBoard(BoardForm boardForm) throws Exception {
-		return sqlSession.insert(NAMESPACE + ".insertBoard", boardForm);
-	}
-	
-	public int deleteBoard(BoardForm boardForm) throws Exception {
-		return sqlSession.delete(NAMESPACE + ".deleteBoard", boardForm);
-	}
-
-	public int updateBoard(BoardForm boardForm) throws Exception {
-		return sqlSession.update(NAMESPACE + ".updateBoard", boardForm);
-	}
-	
-	public int insertBoardReply(BoardForm boardForm) throws Exception {
-		return sqlSession.insert(NAMESPACE + ".insertBoardReply", boardForm);
-	}
-	
 }
